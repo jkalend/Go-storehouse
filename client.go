9b4a5326cb97cff221a25b3eaa3131d8937d78b0
kalen <jkalend@gmail.com>
2025-02-04 02:33:04 +0100

    client update

--- client.go
+++ client.go
@@ -2,30 +2,43 @@ package main
 
 import (
 	"context"
-	"first/microservices-system/order/orderservice"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"storehouse/microservices-system/order/orderservice"
 
 	"google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
 
 	client := orderservice.NewOrderServiceClient(conn)
-	response, err := client.PlaceOrder(context.Background(), &orderservice.OrderRequest{
-		OrderId:    "123",
-		CustomerId: "cust01",
-		Items: []*orderservice.OrderItem{
-			{ProductId: "prod01", Quantity: 2},
-		},
+	response, err := client.GetResponse(context.Background(), &orderservice.OrderResponse{
+		Status:  "SUCCESS",
+		Message: "Order placed successfully",
 	})
 	if err != nil {
 		log.Fatalf("Error calling PlaceOrder: %v", err)
 	}
+	//response, err := client.PlaceOrder(context.Background(), &orderservice.OrderRequest{
+	//	OrderId:    "123",
+	//	CustomerId: "cust01",
+	//	Items: []*orderservice.OrderItem{
+	//		{ProductId: "prod01", Quantity: 2},
+	//	},
+	//})
+	//if err != nil {
+	//	log.Fatalf("Error calling PlaceOrder: %v", err)
+	//}
 
 	log.Printf("Order Response: Status=%s, Message=%s", response.Status, response.Message)
 }
